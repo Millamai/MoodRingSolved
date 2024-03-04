50c196bb2dbfc41ed77e2f56e72de72eb50668ee
Camilla Mai Ryskjær <CARY@zealand.dk>
2024-03-04 09:56:01 +0100

    Add project files.

---
+++ MoodRingSolved/MoodRing.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodRingSolved
+{
+    public class MoodRing
+    {
+
+        public Moods Mood { get; set; }
+        private  Random rnd = new Random();
+        public enum Moods
+        {
+            Default,
+            Creative,
+            Romantic,
+            Uncertain,
+            Comfortable
+        }
+
+
+        public MoodRing()
+        {
+            //Her sættes ringen til at være default
+            Mood = Moods.Default;
+        }
+
+        //Finder et random humør
+        public Moods FindMyMood() {
+            Type type = typeof(Moods);
+            Array values = type.GetEnumValues();
+            return (Moods)values.GetValue(rnd.Next(values.Length));
+        }
+    }
+}
---
+++ MoodRingSolved/Program.cs
@@ -0,0 +1,50 @@
+﻿using static MoodRingSolved.MoodRing;
+
+namespace MoodRingSolved
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MoodRing mr = new MoodRing();
+            Moods m = mr.FindMyMood();
+            DrawRing(m);
+            Console.ReadKey();
+        }
+
+        static void DrawRing(Moods mood) 
+        {
+            switch (mood)
+            {
+                case Moods.Default:
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case Moods.Creative:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case Moods.Romantic:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case Moods.Uncertain:
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    break;
+                case Moods.Comfortable:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+            }
+            Console.WriteLine(@"     __");
+            Console.WriteLine(@"    /  \");
+            Console.WriteLine(@"   _\  /_");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(@"  / ____ \");
+            Console.WriteLine(@" / /    \ \");
+            Console.WriteLine(@"/ /      \ \");
+            Console.WriteLine(@"\ \      / /");
+            Console.WriteLine(@" \ \____/ /");
+            Console.WriteLine(@"  \______/");
+        }
+    }
+}
